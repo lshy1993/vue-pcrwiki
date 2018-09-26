ae688e1d2b7c04b7bcff56f1f653008209df06a8
lshy1993 <lshy1993@live.com>
2018-09-26 19:37:56 +0900

    init

--- src/components/about.vue
+++ src/components/about.vue
@@ -2,9 +2,11 @@
 <div>
     <h2>关于</h2>
     <div>
-        本页面是基于vue+webpack制作的pcrwiki<br>
-        数据来源于游戏内部数据库，版权归cygames所有<br>
-        如果发现有什么bug和需要改进之处，请前往<a href="https://discord.gg/2RUdmNd">discord</a>参与讨论
+        <ul>
+            <li>本页面是基于vue+webpack制作的pcrwiki</li>
+            <li>数据来源于游戏内部数据库，版权归cygames所有</li>
+            <li>如果发现有什么bug和需要改进之处，请前往<a href="https://discord.gg/2RUdmNd">discord</a>参与讨论</li>
+        </ul>
     </div>
 </div>
 </template>
